268c6b78451cc2f7683480c6133c3a4221277bcb
smellilac <littleklyksa@gmail.com>
2024-07-21 14:49:11 +0300

    Added underscores

--- GeneratedFiles/gen.cs
+++ GeneratedFiles/gen.cs
@@ -1,1 +1,1 @@
-namespace QuickEdit; static partial class Builtin { public static long CompileTime = 638571590610704332 ; }
+namespace QuickEdit; static partial class Builtin { public static long CompileTime = 638_571_590_610_704_332 ; }
