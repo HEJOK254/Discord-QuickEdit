7f8c7d91f2e1196eecf68eb68175f256995b446e
smellilac <dimatega@gmail.com>
2024-07-21 18:23:06 +0300

    gen.cs

--- GeneratedFiles/gen.cs
+++ GeneratedFiles/gen.cs
@@ -1,1 +1,2 @@
-namespace QuickEdit { static partial class Builtin { public static long CompileTime = 638571716455080101 ; } }
+namespace QuickEdit; 
+static partial class Builtin { public static long CompileTime = 638_571_716_455_080_101 ; } 
