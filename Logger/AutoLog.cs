384889a4da8850489d145b5d672c946de6c75a3c
smellilac <dimatega@gmail.com>
2024-08-22 16:01:57 +0200

    Change logger namespace
    Co-authored-by: Jakub Dębski <90698026+HEJOK254@users.noreply.github.com>

--- Logger/AutoLog.cs
+++ Logger/AutoLog.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace QuickEdit.LoggerConfig.LoggerImplementation;
+namespace QuickEdit.Logger;
 
 public class AutoLog
 {
--- Logger/SerilogConfiguration.cs
+++ Logger/SerilogConfiguration.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace QuickEdit.LoggerConfig;
+namespace QuickEdit.Logger;
 
 public class SerilogConfiguration
 {
