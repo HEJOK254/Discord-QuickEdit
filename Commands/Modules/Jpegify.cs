7b42e407b0fed97f1e7bb05e0aaf1713bc37d67d
Filip55561 <88946851+Filip55561@users.noreply.github.com>
2024-11-28 00:00:38 +0100

    Ran Formatting

--- Commands/Modules/Jpegify.cs
+++ Commands/Modules/Jpegify.cs
@@ -9,31 +9,31 @@ namespace QuickEdit.Commands.Modules;
 [Group("image", "Image stuff")]
 [IntegrationType(ApplicationIntegrationType.UserInstall)]
 [CommandContextType(InteractionContextType.Guild, InteractionContextType.PrivateChannel)]
-public class Jpegify : InteractionModuleBase 
+public class Jpegify : InteractionModuleBase
 {
     [SlashCommand("jpegify", "Jpegify an Image")]
 
     public async Task JpegifyAsync(
-		[Summary(description: "The image to jpegify")] Discord.Attachment image,
-		[Summary("jpegification", "How much compression should be applied 1 - 100")] int jpegification,
-		[Summary(description: "A message to send with the image when it's jpegified")] string message = "",
-		[Summary(description: "If the image should be sent as a temporary message, that's only visible to you")] bool ephemeral = false)
-	{
+        [Summary(description: "The image to jpegify")] Discord.Attachment image,
+        [Summary("jpegification", "How much compression should be applied 1 - 100")] int jpegification,
+        [Summary(description: "A message to send with the image when it's jpegified")] string message = "",
+        [Summary(description: "If the image should be sent as a temporary message, that's only visible to you")] bool ephemeral = false)
+    {
         jpegification = 101 - Math.Clamp(jpegification, 1, 100);
 
         string imageInputPath = Path.GetTempFileName();
-		string imageOutputPath = Path.Combine(Path.GetTempPath(), imageInputPath + ".jpeg");
+        string imageOutputPath = Path.Combine(Path.GetTempPath(), imageInputPath + ".jpeg");
 
         await DeferAsync(ephemeral);
-        try 
+        try
         {
             Log.Debug("Jpegification: " + jpegification);
             await DownloadImageAsync(image.Url, imageInputPath);
 
             // Process IMG
             using var img = SixLabors.ImageSharp.Image.Load(imageInputPath);
-            var encoder = new JpegEncoder 
-            { 
+            var encoder = new JpegEncoder
+            {
                 Quality = jpegification
             };
             await img.SaveAsJpegAsync(imageOutputPath, encoder);
@@ -53,11 +53,11 @@ public class Jpegify : InteractionModuleBase
     }
 
     private static async Task DownloadImageAsync(string uri, string path)
-	{
-		using var client = new HttpClient();
-		using var s = await client.GetStreamAsync(uri);
-		using var fs = new FileStream(path, FileMode.OpenOrCreate);
-		await s.CopyToAsync(fs);
-		fs.Close();
-	}
+    {
+        using var client = new HttpClient();
+        using var s = await client.GetStreamAsync(uri);
+        using var fs = new FileStream(path, FileMode.OpenOrCreate);
+        await s.CopyToAsync(fs);
+        fs.Close();
+    }
 }
